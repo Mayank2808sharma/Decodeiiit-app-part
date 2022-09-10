c65a877f4cf76a1a3601f2773f94d01e0b073838
Mayank2808sharma <E21CSEU0886@bennett.edu.in>
2022-09-10 23:16:22 +0530

    completing the app part

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+// Get the modal, its content <div>, the button to open it, and the span to close it.
+var modal = document.getElementById("myModal");
+var modalContent = document.querySelector(".modalp");
+var span = document.getElementsByClassName("close")[0];
+var menu = document.getElementById("nav-content");
+
+// When the user clicks the button, modal opens, fetches JokeAPI, and displays it in the modal on screen.
+function jokey() {
+  fetch(
+    "https://v2.jokeapi.dev/joke/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      if (data.type === "single") {
+        var joke = data.joke;
+        modalContent.innerHTML = joke;
+        localStorage.setItem("joke1", JSON.stringify(joke));
+        console.log(joke);
+      } else if (data.type === "twopart") {
+        modalContent.innerHTML = data.setup + "<br><br>" + data.delivery;
+        localStorage.setItem(
+          "joke2",
+          JSON.stringify(data.setup, "<br><br>", data.delivery)
+        );
+        console.log(data.setup, "<br><br>", data.delivery);
+      }
+    });
+  menu.style.transform = "translate(-150px)";
+  modal.style.display = "block";
+
+  span.onclick = function () {
+    modal.style.display = "none";
+    // menu.style.transform = "translate(0px)"
+    location.reload();
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      // menu.style.transform = "translate(0px)"
+      location.reload();
+    }
+  };
+}
+//menu.style.transform = "translate(-150px)";
+const navbar = document.getElementsByClassName("navbar")[0];
+const mapa = document.getElementById("suraklal");
+const lia = document.getElementsByClassName("lia");
+// navbar.style.backgroundColor = "crimson";
+// changing nav bar color on scrolling
+window.addEventListener("scroll", function () {
+  console.log("hello world");
+  if (this.window.scrollY >= 50) {
+    navbar.classList.add("active_nava");
+    mapa.style.color = "white";
+    for (let element of lia) {
+      // console.log(element);
+      element.addEventListener("mouseover", function () {
+        element.style.color = "crimson";
+      });
+      element.addEventListener("mouseout", function () {
+        element.style.color = "white";
+      });
+    }
+  } else {
+    navbar.classList.remove("active_nava");
+    mapa.style.color = "crimson";
+    for (let element of lia) {
+      // console.log(element);
+      element.addEventListener("mouseover", function () {
+        element.style.color = "crimson";
+      });
+      element.addEventListener("mouseout", function () {
+        element.style.color = "white";
+      });
+    }
+  }
+});
